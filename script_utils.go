df1b148a98da830decd1545bfe20ddf36a8737a2
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2018-12-27 18:48:28 +0200

    added createSettleWithGuestTx func

--- script_utils.go
+++ script_utils.go
@@ -65,3 +65,39 @@ func createSettleOnlyWithHostTx(
 		),
 	)
 }
+
+func createSettleWithGuestTx(
+	rsn,
+	paymentTime uint64,
+	guestAddress,
+	guestAmount,
+	escrowAddress string,
+) (
+	*build.TransactionBuilder,
+	error,
+) {
+
+	return build.Transaction(
+		build.TestNetwork,
+		build.SourceAccount{AddressOrSeed: escrowAddress},
+		build.Sequence{Sequence: rsn + 2},
+		build.Timebounds{
+			MinTime: paymentTime + 2*defaultFinalityDelay + defaultMaxRoundDuration,
+		},
+		// Pay GuestAmount from EscrowAccount to GuestAccount
+		build.Payment(
+			build.Destination{AddressOrSeed: guestAddress},
+			build.NativeAmount{Amount: guestAmount},
+		),
+	)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	//txe, err := tx.Sign(host.escrowKeyPair.Seed())
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	//return &txe, nil
+}
