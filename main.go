6b39e9e32842c370405be9312c83482416447a07
evgeniy.scherbina <evgeniy.scherbina@bitfury.com>
2018-12-27 17:28:52 +0200

    refactored

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/stellar/go/build"
+	"github.com/stellar/go/clients/horizon"
+
 	//"github.com/stellar/go/build"
 	//"github.com/stellar/go/clients/horizon"
 	"log"
@@ -14,59 +17,59 @@ func main() {
 	payment := flag.Bool("payment", false, "")
 	flag.Parse()
 
+	fmt.Println("starlight_demo")
+
+	fmt.Println("creating host account:")
+	hostAccount, err := newHostAccount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("balance: %v\n\n", hostAccount.loadBalance())
+
+	fmt.Println("creating channel accounts:")
+	fmt.Printf("creating: %v\n", hostRatchetAccount)
+	if err := hostAccount.setupAccountTx(hostRatchetAccount); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.hostRatchetAccount.keyPair.Address()))
+
+	fmt.Printf("creating: %v\n", guestRatchetAccount)
+	if err := hostAccount.setupAccountTx(guestRatchetAccount); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.guestRatchetAccount.keyPair.Address()))
+
+	fmt.Printf("creating: %v\n", escrowAccount)
+	if err := hostAccount.setupAccountTx(escrowAccount); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.escrowKeyPair.Address()))
+
+	fmt.Println("creating guest account:")
+	guestAccount, err := newGuestAccount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("balance: %v\n\n", loadBalance(guestAccount.keyPair.Address()))
+
+	channelProposeMsg := hostAccount.createChannelProposeMsg(guestAccount.keyPair.Address())
+
+	fmt.Println("receiveChannelProposeMsg: ")
+	channelAcceptMsg, err := guestAccount.receiveChannelProposeMsg(channelProposeMsg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("publish fundingTx")
+	if err := hostAccount.fundingTx(guestAccount.keyPair.Address()); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("host account's balance(before force close): %v\n\n", hostAccount.loadBalance())
+
+	rsn := roundSequenceNumber(hostAccount.baseSequenceNumber, 1)
+
 	if *unilateralClose {
-		fmt.Println("starlight_demo")
-
-		fmt.Println("creating host account:")
-		hostAccount, err := newHostAccount()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("balance: %v\n\n", hostAccount.loadBalance())
-
-		fmt.Println("creating channel accounts:")
-		fmt.Printf("creating: %v\n", hostRatchetAccount)
-		if err := hostAccount.setupAccountTx(hostRatchetAccount); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.hostRatchetAccount.keyPair.Address()))
-
-		fmt.Printf("creating: %v\n", guestRatchetAccount)
-		if err := hostAccount.setupAccountTx(guestRatchetAccount); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.guestRatchetAccount.keyPair.Address()))
-
-		fmt.Printf("creating: %v\n", escrowAccount)
-		if err := hostAccount.setupAccountTx(escrowAccount); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("balance: %v\n\n", loadBalance(hostAccount.escrowKeyPair.Address()))
-
-		fmt.Println("creating guest account:")
-		guestAccount, err := newGuestAccount()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("balance: %v\n\n", loadBalance(guestAccount.keyPair.Address()))
-
-		channelProposeMsg := hostAccount.createChannelProposeMsg(guestAccount.keyPair.Address())
-
-		fmt.Println("receiveChannelProposeMsg: ")
-		channelAcceptMsg, err := guestAccount.receiveChannelProposeMsg(channelProposeMsg)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("publish fundingTx")
-		if err := hostAccount.fundingTx(guestAccount.keyPair.Address()); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("host account's balance(before force close): %v\n\n", hostAccount.loadBalance())
-
-		rsn := roundSequenceNumber(hostAccount.baseSequenceNumber, 1)
-
 		fmt.Println("publish ratchetTx")
 		tx, err := hostAccount.createAndSignRatchetTxForSelf(
 			channelAcceptMsg.GuestRatchetRound1Sig,
@@ -95,87 +98,88 @@ func main() {
 	}
 
 	if *payment {
-		//paymentProposeMsg, err := hostAccount.createPaymentProposeMsg(1, guestAccount.keyPair.Address())
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
+		paymentProposeMsg, err := hostAccount.createPaymentProposeMsg(1, guestAccount.keyPair.Address())
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		//msg *PaymentProposeMsg,
 		//escrowAddress,
 		//hostRatchetAddress string,
 		//bsn int64,
-		//paymentAcceptMsg, err := guestAccount.receivePaymentProposeMsg(
-		//	paymentProposeMsg,
-		//	hostAccount.escrowKeyPair.Address(),
-		//	hostAccount.hostRatchetAccount.keyPair.Address(),
-		//	int64(hostAccount.baseSequenceNumber),
-		//)
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
+		paymentAcceptMsg, err := guestAccount.receivePaymentProposeMsg(
+			paymentProposeMsg,
+			hostAccount.escrowKeyPair.Address(),
+			hostAccount.hostRatchetAccount.keyPair.Address(),
+			int64(hostAccount.baseSequenceNumber),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		//fmt.Println("RATCHET TX")
-		//txCopy := paymentAcceptMsg.RecipientRatchetSig
-		//txCopy.Mutate(build.Sign{Seed: hostAccount.escrowKeyPair.Seed()})
-		//if err := hostAccount.publishTx(paymentAcceptMsg.RecipientRatchetSig); err != nil {
-		//	fmt.Println("tx fail")
-		//	err2 := err.(*horizon.Error).Problem
-		//	fmt.Println("Type: ", err2.Type)
-		//	fmt.Println("Title: ", err2.Title)
-		//	fmt.Println("Status: ", err2.Status)
-		//	fmt.Println("Detail:", err2.Detail)
-		//	fmt.Println("Instance: ", err2.Instance)
-		//	for key, value := range err2.Extras {
-		//		fmt.Println("KEYVALUE: ", key, string(value))
-		//	}
-		//	// fmt.Println("Extras: ",   err2.Extras)
-		//	log.Fatal(err)
-		//}
-		//
-		//fmt.Println("SETTLE WITH GUEST TX")
-		//
-		//fmt.Println("WAIT")
-		//time.Sleep((2*defaultFinalityDelay + defaultMaxRoundDuration) * time.Second + 10 * time.Second)
-		//
-		//txCopy = paymentAcceptMsg.RecipientSettleWithGuestSig
-		//// txCopy.Mutate(build.Sign{Seed:})
-		//_ = txCopy
-		//if err := hostAccount.publishTx(paymentAcceptMsg.RecipientSettleWithGuestSig); err != nil {
-		//	fmt.Println("tx fail")
-		//	err2 := err.(*horizon.Error).Problem
-		//	fmt.Println("Type: ", err2.Type)
-		//	fmt.Println("Title: ", err2.Title)
-		//	fmt.Println("Status: ", err2.Status)
-		//	fmt.Println("Detail:", err2.Detail)
-		//	fmt.Println("Instance: ", err2.Instance)
-		//	for key, value := range err2.Extras {
-		//		fmt.Println("KEYVALUE: ", key, string(value))
-		//	}
-		//	// fmt.Println("Extras: ",   err2.Extras)
-		//	log.Fatal(err)
-		//}
-		//
-		//fmt.Println("SETTLE WITH HOST TX")
-		//txCopy = paymentAcceptMsg.RecipientSettleWithHostSig
-		//fmt.Println(len(txCopy.E.Signatures))
-		//fmt.Println(txCopy.E.Signatures)
-		//_ = txCopy
-		//if err := hostAccount.publishTx(paymentAcceptMsg.RecipientSettleWithHostSig); err != nil {
-		//	fmt.Println("tx fail")
-		//	err2 := err.(*horizon.Error).Problem
-		//	fmt.Println("Type: ", err2.Type)
-		//	fmt.Println("Title: ", err2.Title)
-		//	fmt.Println("Status: ", err2.Status)
-		//	fmt.Println("Detail:", err2.Detail)
-		//	fmt.Println("Instance: ", err2.Instance)
-		//	for key, value := range err2.Extras {
-		//		fmt.Println("KEYVALUE: ", key, string(value))
-		//	}
-		//	// fmt.Println("Extras: ",   err2.Extras)
-		//	log.Fatal(err)
-		//}
+		fmt.Println("RATCHET TX")
+		txCopy := paymentAcceptMsg.RecipientRatchetSig
+		txCopy.Mutate(build.Sign{Seed: hostAccount.escrowKeyPair.Seed()})
+		if err := hostAccount.publishTx(paymentAcceptMsg.RecipientRatchetSig); err != nil {
+			fmt.Println("tx fail")
+			err2 := err.(*horizon.Error).Problem
+			fmt.Println("Type: ", err2.Type)
+			fmt.Println("Title: ", err2.Title)
+			fmt.Println("Status: ", err2.Status)
+			fmt.Println("Detail:", err2.Detail)
+			fmt.Println("Instance: ", err2.Instance)
+			for key, value := range err2.Extras {
+				fmt.Println("KEYVALUE: ", key, string(value))
+			}
+			// fmt.Println("Extras: ",   err2.Extras)
+			log.Fatal(err)
+		}
 
-		//fmt.Printf("guest account's balance(after force close): %v\n\n", loadBalance(guestAccount.keyPair.Address()))
-		//_ = channelAcceptMsg
+		fmt.Println("SETTLE WITH GUEST TX")
+
+		fmt.Println("WAIT")
+		time.Sleep((2*defaultFinalityDelay + defaultMaxRoundDuration) * time.Second + 10 * time.Second)
+
+		txCopy = paymentAcceptMsg.RecipientSettleWithGuestSig
+		// txCopy.Mutate(build.Sign{Seed:})
+		_ = txCopy
+		if err := hostAccount.publishTx(paymentAcceptMsg.RecipientSettleWithGuestSig); err != nil {
+			fmt.Println("tx fail")
+			err2 := err.(*horizon.Error).Problem
+			fmt.Println("Type: ", err2.Type)
+			fmt.Println("Title: ", err2.Title)
+			fmt.Println("Status: ", err2.Status)
+			fmt.Println("Detail:", err2.Detail)
+			fmt.Println("Instance: ", err2.Instance)
+			for key, value := range err2.Extras {
+				fmt.Println("KEYVALUE: ", key, string(value))
+			}
+			// fmt.Println("Extras: ",   err2.Extras)
+			log.Fatal(err)
+		}
+
+		fmt.Println("SETTLE WITH HOST TX")
+		txCopy = paymentAcceptMsg.RecipientSettleWithHostSig
+		fmt.Println(len(txCopy.E.Signatures))
+		fmt.Println(txCopy.E.Signatures)
+		_ = txCopy
+		if err := hostAccount.publishTx(paymentAcceptMsg.RecipientSettleWithHostSig); err != nil {
+			fmt.Println("tx fail")
+			err2 := err.(*horizon.Error).Problem
+			fmt.Println("Type: ", err2.Type)
+			fmt.Println("Title: ", err2.Title)
+			fmt.Println("Status: ", err2.Status)
+			fmt.Println("Detail:", err2.Detail)
+			fmt.Println("Instance: ", err2.Instance)
+			for key, value := range err2.Extras {
+				fmt.Println("KEYVALUE: ", key, string(value))
+			}
+			// fmt.Println("Extras: ",   err2.Extras)
+			log.Fatal(err)
+		}
+
+		fmt.Printf("host account's balance(after force close): %v\n\n", hostAccount.loadBalance())
+		fmt.Printf("guest account's balance(after force close): %v\n\n", loadBalance(guestAccount.keyPair.Address()))
+		_ = channelAcceptMsg
 	}
 }
